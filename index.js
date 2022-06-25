a1245297b436c226fc965ec84fc1617e00c87e8e
Wilmar Rodriguez <rwilmar@gmail.com>
2022-06-25 22:57:51 +0200

    routes and shortener middleware implemented

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import express from 'express';
-
+import { getRegister, getUrl, saveUrl } from './persistence.js';
 
 const APP_PORT = 3000;
 
@@ -11,11 +11,91 @@ app.use(express.urlencoded({ extended: true }));
 
 
 const welcomeMessage = (req, res) => {
-  res.status(200).send({ message: "Welcome to the Mars-Command API." });
+  res.status(200).send({ message: "Welcome to the URL-Shortener API." });
 };
 
+/**
+ * shorten url middleware
+ * @param {express.Request} req - express request object
+ * @param {object} req.body - http body object
+ * @param {string} req.body.url - url in string format 
+ * @param {express.Response} res - express res object
+ * @returns {Promise<void>}
+ */
+const shortenUrl = async (req, res) => {
+  try{
+    const url = req.body.url;
+    if(!url) throw new Error('url empty or not found in body');
+    const shortenPath = await saveUrl(url, req.ip);
+    return res.status(201).send({
+      isOk: true,
+      shortenPath,
+    });
+  }
+  catch(error){
+    res.status(500).send({
+      isOk: false,
+      message: 'the system was not able to shorten the url, Error: '+error.message,
+    });
+  }
+}
+
+/**
+ * get url details middleware
+ * @param {express.Request} req - express request object
+ * @param {object} req.params - http request url params object
+ * @param {string} req.body.hash - url in string format 
+ * @param {express.Response} res - express res object
+ * @returns {Promise<void>}
+ */
+ const getUrlAddress = async (req, res) => {
+  try{
+    const hash = req.params.hash;
+    if(!hash) throw new Error('hash empty or not found in parameter');
+    const reg = await getRegister(hash);
+    return res.status(201).send({
+      url: reg.url,
+      created: new Date(reg.timestamp),
+      client: reg.ipAddress,
+    });
+  }
+  catch(error){
+    res.status(500).send({
+      isOk: false,
+      message: 'The system was not able to get the url, Error: '+error.message,
+    });
+  }
+}
+
+/**
+ * get url details middleware
+ * @param {express.Request} req - express request object
+ * @param {object} req.params - http request url params object
+ * @param {string} req.body.hash - url in string format 
+ * @param {express.Response} res - express res object
+ * @returns {Promise<void>}
+ */
+ const redirToHash = async (req, res) => {
+  try{
+    const hash = req.params.hash;
+    if(!hash) throw new Error('hash empty or not found in parameter');
+    const newUrl = await getUrl(hash);
+    return res.redirect(newUrl);
+  }
+  catch(error){
+    res.status(500).send({
+      isOk: false,
+      message: 'The system was not able to get the url, Error: '+error.message,
+    });
+  }
+}
+
 app.get('/', welcomeMessage);
+app.post('/shorten', shortenUrl);
+app.get('/:hash/details', getUrlAddress);
+app.get('/:hash', redirToHash);
+
 
 app.listen(APP_PORT, () => {
-  console.log(`shortening url service App listening on port: ${APP_PORT}`)
+  console.log(`Shortening url service App listening on port: ${APP_PORT}`)
 })
